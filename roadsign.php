68b0bb1d2c0b3044e8975660b25de6bac9ba1f19
Daniel Duris <dusoft@gmail.com>
2020-08-18 16:05:15 +0200

    mpdf upgrade + transparency

--- roadsign.php
+++ roadsign.php
@@ -1,13 +1,18 @@
 <?php
 if (!isset($_GET['sign']) or !isset($_GET['type']) or !isset($_GET['number']) or ($_GET['sign'] != 'route' and $_GET['sign'] != 'direction') or ($_GET['type'] != 'straight' and $_GET['type'] != 'left' and $_GET['type'] != 'right')) {
     $image = imagecreatefrompng('sources/route-straight.png');
-    imagettftext($image, 100, 0, 700, 300, 0, 'fonts/Tern-Regular.ttf', 'chyba');
+    $transparent = imagecolorallocate($image, 0, 0, 0);
+    imagecolortransparent($image, $transparent);
+    $color = imagecolorallocate($image, 170, 0, 0);
+    imagettftext($image, 200, 0, 550, 400, $color, 'fonts/Tern-Regular.ttf', 'chyba');
     header('Content-Type: image/png');
     imagepng($image);
     imagedestroy($image);
 }
 if ($_GET['sign'] == 'route') {
     $image = imagecreatefrompng('sources/route-' . $_GET['type'] . '.png');
+    $transparent = imagecolorallocate($image, 0, 0, 0);
+    imagecolortransparent($image, $transparent);
     $color = imagecolorallocate($image, 28, 107, 61);
     $image_size = 1770;
     $font_size = 300;
@@ -23,6 +28,8 @@ if ($_GET['sign'] == 'route') {
     imagettftext($image, $font_size, 0, $x, 500, $color, 'fonts/Tern-Regular.ttf', $_GET['number']);
 } elseif ($_GET['sign'] == 'direction') {
     $image = imagecreatefrompng('sources/direction-' . $_GET['type'] . '.png');
+    $transparent = imagecolorallocate($image, 0, 0, 0);
+    imagecolortransparent($image, $transparent);
     $color = imagecolorallocate($image, 28, 107, 61);
     $image_size = 2000;
     $font_size = 120;
